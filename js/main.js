5ae7e490863f3f7e3016f4c0ffdf55a758eac319
syruii <stevevytran@gmail.com>
2018-04-04 22:16:10 +1000

    add panels, and remove panels separate saturation grid row numbers key shortcuts

--- js/main.js
+++ js/main.js
@@ -14,34 +14,33 @@ var saturationColors;
 var flyoutXML = '\
 <Menu> \
 	<MenuItem Id="configure" Label="Configure" Enabled="true" Checked="false"/> \
-	\
+	<MenuItem Label="---" /> \
+	<MenuItem Id="hsbFlag" Label="HSB Saturation" Enabled="true" Checked="false"/> \
 </Menu>';
 
 var settingsShown = false;
 
 // generic callback
-
 cs.addEventListener("com.adobe.csxs.events.ThemeColorChanged", setAppTheme);
 cs.addEventListener("com.adobe.PhotoshopJSONCallback" + gExtensionID, PhotoshopCallbackUnique);
+// keydown callback
+cs.addEventListener('keydown', keyListener);
 // callback for flyout
 cs.addEventListener("com.adobe.csxs.events.flyoutMenuClicked", flyoutMenuHandler)
 
 function onLoaded() {
     //　テーマを初期設定
     setAppTheme(null);
-    Persistent(true);
+    Persistent(false);
     //　テーマが変わった時のイベントに登録
     cs, addEventListener(CSInterface.THEME_COLOR_CHANGED_EVENT, setAppTheme);
     // 'r' 押下されると page reload
-    document.onkeypress = function (e) {
-        if (e.charCode == 114) {
-            window.location.reload();
-        }
-    }
+
     Register(true, eventSet.toString());
     cs.setPanelFlyoutMenu(flyoutXML);
 
     // set settings
+    loadSettings();
     $('#saturation').val(Settings.saturationMaxStep * 10);
     $('#temperature').val(Settings.tempStepDistance);
     $('#luminance').val(Settings.lumaStepDistance);
@@ -104,49 +103,48 @@ function Register(inOn, inEvents) {
 }
 
 function PhotoshopCallbackUnique(csEvent) {
-    try {
-        if (typeof csEvent.data === "string") {
-            // color changed
-            // check if color differs from our calculated centre color
-            var eventData = csEvent.data.replace("ver1,{", "{");
-            var eventDataParse = JSON.parse(eventData);
-            //var jsonStringBack = JSON.stringify(eventDataParse);
-            //JSLogIt(jsonStringBack);
+    if (typeof csEvent.data === "string") {
+        // color changed
+        // check if color differs from our calculated centre color
+        var eventData = csEvent.data.replace("ver1,{", "{");
+        var eventDataParse = JSON.parse(eventData);
+        //var jsonStringBack = JSON.stringify(eventDataParse);
+        //JSLogIt(jsonStringBack);
 
-            // no type coercion on switch statement
-            var col = eventDataParse.eventData.to;
-            var convert = cep_node.require('color-convert');
-            if (col) {
-                switch (col._obj) {
-                    case "HSBColorClass":
-                        var rgb = convert.hsl.rgb(col.hue._value, col.saturation, col.brightness);
-                        fgcolor = convert.rgb.hex(rgb);
-                        break;
-                    case "RGBColor":
-                        fgcolor = convert.rgb.hex(col.red, col.grain, col.blue);
-                        break;
-                    case "CMYKColorClass":
-                        var rgb = convert.cmyk.rgb(col.cyan, col.magenta, col.yellowColor, col.black);
-                        fgcolor = convert.rgb.hex(rgb);
-                        break;
-                    case "labColor":
-                        var xyz = convert.lab.xyz.raw(col.luminance, col.a, col.b);
-                        var rgb = convert.xyz.rgb(xyz);
-                        fgcolor = convert.rgb.hex(rgb);
-                        break;
-                    //default:
-                        //JSLogIt("PhotoshopCallbackUnique received unexpected value for color obj" + col._obj);
-                }
-                fgcolor = '#' + fgcolor;
+        // no type coercion on switch statement
+        var col = eventDataParse.eventData.to;
+        var convert = cep_node.require('color-convert');
+        var newfg;
+        if (col) {
+            switch (col._obj) {
+                case "HSBColorClass":
+                    var rgb = convert.hsl.rgb(col.hue._value, col.saturation, col.brightness);
+                    newfg = convert.rgb.hex(rgb);
+                    break;
+                case "RGBColor":
+                    newfg = convert.rgb.hex(col.red, col.grain, col.blue);
+                    break;
+                case "CMYKColorClass":
+                    var rgb = convert.cmyk.rgb(col.cyan, col.magenta, col.yellowColor, col.black);
+                    newfg = convert.rgb.hex(rgb);
+                    break;
+                case "labColor":
+                    var xyz = convert.lab.xyz.raw(col.luminance, col.a, col.b);
+                    var rgb = convert.xyz.rgb(xyz);
+                    newfg = convert.rgb.hex(rgb);
+                    break;
+                //default:
+                //JSLogIt("PhotoshopCallbackUnique received unexpected value for color obj" + col._obj);
+            }
+            newfg = '#' + newfg;
+            if (newfg !== fgcolor) {
+                fgcolor = newfg;
                 refresh();
             }
-        } else {
-            JSLogIt("PhotoshopCallbackUnique expecting string for csEvent.data!");
         }
-    } catch (e) {
-        JSLogIt("PhotoshopCallbackUnique catch:" + e);
     }
 }
+
 
 // log errors
 function JSLogIt(inMessage) {
@@ -154,20 +152,121 @@ function JSLogIt(inMessage) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// Grid panel functions
+// Key Listener & Storage
 ///////////////////////////////////////////////////////////////////////////////
-var Settings = {
-    showSaturation: true,
-    tempStepDistance: 12,
-    lumaStepDistance: 12,
-    saturationMaxStep: 1,
-    saturationRawDistanceMin: 2,
-    saturationRawDistanceMax: 8,
-    saturationHSB: false,
-    oneSideQuantity: 3,
-    rawQuantity: 7
+
+cs.registerKeyEventsInterest(JSON.stringify([
+    { "keyCode": 82},
+    { "keyCode": 83},
+    { "keyCode": 81},
+    { "keyCode": 81, "shiftKey": true},
+    { "keyCode": 69},
+    { "keyCode": 69, "shiftKey": true},
+]));
+
+window.addEventListener('keydown', keyListener);
+
+// listen to key inputs when in focus
+function keyListener(e) {
+    switch (e.key.toLowerCase()) {
+        case "r":
+            window.location.reload();
+            break;
+        case "s":
+            Settings.showSaturation =  !Settings.showSaturation;
+            toggleSaturation();
+            break;
+        case "q":
+            // either reduce temp plates or saturation plates
+            if (e.shiftKey) {
+                // reduce saturation panels
+                if (Settings.oneSideSaturationQuantity > 2) {
+                    Settings.oneSideSaturationQuantity--;
+                    Settings.rawSaturationQuantity = (Settings.oneSideSaturationQuantity * 2) + 1;
+                    document.documentElement.style.setProperty('--sat-rows', `${Settings.rawSaturationQuantity}`);
+                    $('#saturation-grid').find(".grid-item:gt(" + (Settings.rawSaturationQuantity - 1) +")").hide();
+                    refreshSaturation();
+                }
+            } else {
+                // reduce temperature panels
+                if (Settings.oneSideQuantity > 2) {
+                    Settings.oneSideQuantity--;
+                    Settings.rawQuantity = (Settings.oneSideQuantity * 2) + 1;
+                    document.documentElement.style.setProperty('--temp-columns', `${Settings.rawQuantity}`);
+                    $('#temperature-grid').find(".grid-item:gt(" + ( Math.pow(Settings.rawQuantity, 2) - 1) +")").hide();
+                    refreshColors();
+                }
+            }
+            break;
+        case "e":
+            if (e.shiftKey) {
+                // increase saturation panels
+                JSLogIt('or here');
+                if (Settings.rawSaturationQuantity < Settings.maxSaturationQuantity) {
+                    Settings.oneSideSaturationQuantity++;
+                    Settings.rawSaturationQuantity = (Settings.oneSideSaturationQuantity * 2) + 1;
+                    document.documentElement.style.setProperty('--sat-rows', `${Settings.rawSaturationQuantity}`);
+                    $('#saturation-grid').find(".grid-item:lt(" + Settings.rawSaturationQuantity +")").show();
+                    refreshSaturation();
+                }
+            } else {
+                // increase temperature panels
+                if (Settings.rawQuantity < Settings.maxTemperatureQuantity) {
+                    Settings.oneSideQuantity++;
+                    Settings.rawQuantity = (Settings.oneSideQuantity * 2) + 1;
+                    document.documentElement.style.setProperty('--temp-columns', `${Settings.rawQuantity}`);
+                    $('#temperature-grid').find(".grid-item:lt(" + Math.pow(Settings.rawQuantity, 2) +")").show();
+                    refreshColors();
+                }
+            }
+            break;
+    }
+    saveSettings();
 }
 
+var Settings = {
+    maxSaturationQuantity: 15,
+    maxTemperatureQuantity: 11
+};
+
+// load settings on startup, if saturation is hidden, convert the html
+function loadSettings() {
+    Settings.showSaturation = (window.localStorage.showSaturation === undefined ?
+        true: window.localStorage.showSaturation == 'true');
+    Settings.tempStepDistance = Number(window.localStorage.tempStepDistance || 12);
+    Settings.lumaStepDistance = window.localStorage.lumaStepDistance || 12;
+    Settings.saturationMaxStep = window.localStorage.saturationMaxStep || 1;
+    Settings.saturationHSB = (window.localStorage.saturationHSB == 'true');
+
+    Settings.oneSideQuantity = window.localStorage.oneSideQuantity || 3;
+    Settings.oneSideSaturationQuantity = window.localStorage.oneSideSaturationQuantity || 3;
+
+    Settings.rawQuantity = (Settings.oneSideQuantity * 2) + 1;
+    Settings.rawSaturationQuantity = (Settings.oneSideSaturationQuantity * 2) + 1;
+
+    if (!Settings.showSaturation) {
+        toggleSaturation();
+    }
+    document.documentElement.style.setProperty('--temp-columns', `${Settings.rawQuantity}`);
+    document.documentElement.style.setProperty('--sat-rows', `${Settings.rawSaturationQuantity}`);
+    cs.updatePanelMenuItem("HSB Saturation", true, Settings.saturationHSB);
+}
+
+function saveSettings() {
+    window.localStorage.showSaturation = Settings.showSaturation;
+    window.localStorage.tempStepDistance = Settings.tempStepDistance;
+    window.localStorage.lumaStepDistance = Settings.lumaStepDistance;
+    window.localStorage.saturationMaxStep = Settings.saturationMaxStep;
+
+    window.localStorage.oneSideQuantity = Settings.oneSideQuantity;
+    window.localStorage.oneSideSaturationQuantity = Settings.oneSideSaturationQuantity;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Grid panel functions
+///////////////////////////////////////////////////////////////////////////////
 
 // recalculates color panels and saturation
 function refresh() {
@@ -177,22 +276,43 @@ function refresh() {
     }
 }
 
-// populates color vector
-function initColorsVectors() {
-    colors = new Array(Settings.rawQuantity);
-    for (var i = 0; i < Settings.rawQuantity; i++) {
-        colors[i] = new Array(Settings.rawQuantity);
-        for (var j = 0; j < Settings.rawQuantity; j++) {
-            colors[i][j] = fgcolor;
-        }
+function toggleSaturation() {
+    if (Settings.showSaturation) {
+        // hide saturation grid and fix grid
+        // also fix height
+        $('#saturation-grid').show()
+        $('#grid-container').removeClass("grid-container-solo").addClass("grid-container-dual");
+        document.documentElement.style.setProperty('--temp-content-width', '85.75vw');
+        window.__adobe_cep__.resizeContent(window.innerWidth, Math.round(window.innerWidth * 0.9));
+        refreshSaturation();
+    } else {
+        $('#saturation-grid').hide();
+        $('#grid-container').removeClass("grid-container-dual").addClass("grid-container-solo");
+        document.documentElement.style.setProperty('--temp-content-width', '100vw');
+        window.__adobe_cep__.resizeContent(window.innerWidth, window.innerWidth + 20);
     }
 }
 
+// populates color vector
+function initColorsVectors() {
+    colors = new Array(Settings.maxTemperatureQuantity);
+    for (var i = 0; i < Settings.maxTemperatureQuantity; i++) {
+        colors[i] = new Array(Settings.maxTemperatureQuantity);
+        for (var j = 0; j < Settings.maxTemperatureQuantity; j++) {
+            colors[i][j] = fgcolor;
+        }
+    }
+    // hide the unneeded grid items
+    $('#temperature-grid').find(".grid-item:gt(" + ( Math.pow(Settings.rawQuantity, 2) - 1) +")").hide();
+}
+
 function initSaturationVectors() {
-    saturationColors = new Array(Settings.rawQuantity);
-    for (var i = 0; i < Settings.rawQuantity; i++) {
+    saturationColors = new Array(Settings.maxSaturationQuantity);
+    for (var i = 0; i < Settings.maxSaturationQuantity; i++) {
         saturationColors[i] = fgcolor;
     }
+    // hide the unneeded grid items
+    $('#saturation-grid').find(".grid-item:gt(" + (Settings.rawSaturationQuantity - 1) +")").hide();
 }
 
 // replaces bg color of squares with the colors contained in the color array
@@ -201,14 +321,14 @@ function colorSquares() {
     for (var i = 0; i < Settings.rawQuantity; i++) {
         for (var j = 0; j < Settings.rawQuantity; j++) {
             childNum = (Settings.rawQuantity * i) + j + 1;
-            $(`.temperature-grid > div:nth-child(${childNum})`).css("background-color", colors[i][j]);
+            $(`#temperature-grid > div:nth-child(${childNum})`).css("background-color", colors[i][j]);
         }
     }
 }
 
 function colorSaturation() {
-    for (var i = 0; i < Settings.rawQuantity; i++) {
-        $(`.saturation-grid > div:nth-child(${i + 1})`).css("background-color", saturationColors[i]);
+    for (var i = 0; i < Settings.rawSaturationQuantity; i++) {
+        $(`#saturation-grid > div:nth-child(${i + 1})`).css("background-color", saturationColors[i]);
     }
 }
 
@@ -230,16 +350,17 @@ function refreshColors() {
         currentTemp += Settings.tempStepDistance;
         currentLightness = (Settings.lumaStepDistance * Settings.oneSideQuantity);
     }
+
     colorSquares();
 }
 
 function refreshSaturation() {
     var currentSaturationStep = Settings.saturationMaxStep;
-    var saturationDecreaseStep = (Settings.saturationMaxStep / Settings.oneSideQuantity);
+    var saturationDecreaseStep = (Settings.saturationMaxStep / Settings.oneSideSaturationQuantity);
     var convert = require('color-convert')
     var foregroundColor = convert.hex.rgb(fgcolor);
 
-    for (var i = 0; i < Settings.rawQuantity; i++) {
+    for (var i = 0; i < Settings.rawSaturationQuantity; i++) {
         if (Settings.saturationHSB) {
             saturationColors[i] = '#' + convert.rgb.hex(
                 saturationMoveHSB(foregroundColor, currentSaturationStep)
@@ -254,18 +375,18 @@ function refreshSaturation() {
     colorSaturation();
 }
 
-// set click event for nth child in square grid
-$('.temperature-grid').on('click', '> *', setColorFromCSS);
+// set click event for grid-items
+$(document.body).on('click', '.grid-item', setColorFromCSS);
 
-// set click event for nth children in saturation grid
-$('.saturation-grid').on('click', '> *', setColorFromCSS);
 
 function setColorFromCSS(e) {
     // set color and refresh
     // kind of hacky, extract rgb directly from css
     var colorString = e.target.style.backgroundColor;
+    var convert = require('color-convert');
     if (colorString) {
         var rgb = colorString.match(/\d+/g);
+        fgcolor = convert.rgb.hex(rgb);
         cs.evalScript(`setForegroundColorRGB(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`, refresh)
     }
 }
@@ -293,6 +414,13 @@ function flyoutMenuHandler(flyoutEvent) {
             } else {
                 closeSettings();
             }
+            break;
+        case "hsbFlag":
+            Settings.saturationHSB = !Settings.saturationHSB;
+            cs.updatePanelMenuItem("HSB Saturation", true, Settings.saturationHSB);
+            saveSettings();
+            refreshSaturation();
+            break;
     }
 }
 
